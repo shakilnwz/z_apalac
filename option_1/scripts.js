6b27e6abf6d47a401efcad2635dc4325a75be696
Shakil Nawaz <shakilnwz@gmail.com>
2023-11-24 00:44:55 +0600

    nav colapsable menu created

--- option_1/scripts.js
+++ option_1/scripts.js
@@ -1,5 +1,31 @@
-let cs = document.querySelectorAll('.carousel')
-console.log(cs)
-cs.forEach(element => {
-    console.log(element)
-});
+const nav = document.querySelector('#nav-menu')
+const show = document.querySelector('.show')
+const hide = document.querySelector('.hide')
+
+function applyStyle() {
+    nav.style.width = '70%'
+    nav.style.position = "absolute"
+    show.style.width = "0"
+    hide.style.width = "7rem"
+}
+
+function resetStyle() {
+    nav.style.width = "0"
+    nav.style.position = "fixed"
+    show.style.width = "7rem"
+    hide.style.width = "0"
+}
+show.addEventListener('mouseup', applyStyle)
+
+hide.addEventListener('mouseup', resetStyle)
+
+console.log(window.screen.width > 480)
+window.addEventListener('resize', () => {
+    if (window.screen.width > 480) {
+        nav.style.width = "0"
+        nav.style.position = "fixed"
+        show.style.width = "7rem"
+        hide.style.width = "0"
+    }
+
+})
