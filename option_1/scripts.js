59e539aeb64b6a82e8b3f733e3f655c9f70ca197
Shakil Nawaz <shakilnwz@gmail.com>
2023-11-30 00:40:26 +0600

    carousel almost finished

--- option_1/scripts.js
+++ option_1/scripts.js
@@ -51,38 +51,74 @@ call.forEach(item => {
 /**
  * if the parent of carousel width exceeds window width then add carousel auto child to every immediate childs class
  */
-// const carouselParent = document.querySelectorAll('.carousel-auto')
-
-// function scroller() {
-
-// }
-
-//form validation
-const regExps = {
-    email: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/g,
-    phone: /^\+?[0-9()-.\s]{10,}$/g,
-    name: /^[a-zA-Z]+$/g,
-    text: /^[a-zA-Z0-9\s.,;:'"&()!?]+$/g
-}
-
-const inputs = document.querySelectorAll('input, textarea')
-const warning = document.querySelector('.warning')
-
-inputs.forEach(elem => {
-    elem.addEventListener('change', () => {
-        let test = regExps[elem.dataset.validator].test(elem.value)
-        console.log(test)
-
-        if (!test && !elem.value == '') {
-            warning.innerText = `Please enter valid ${elem.dataset.validator}`
-            warning.style.visibility = "initial"
-        } else {
-            warning.style.visibility = "hidden"
-        }
-    })
+const carouselParent = document.querySelector('.carousel')
+const leftItem = carouselParent.querySelector('.left-item')
+const rightItem = carouselParent.querySelector('.right-item')
+const carouselAuto = carouselParent.querySelector('.carousel-auto')
+let carouselChilds = Array.from(carouselAuto.children)
+let width = carouselChilds[0].getBoundingClientRect().width
+console.log(carouselChilds.reduce((x, y) => { return x + y.clientWidth }, 0))
+leftItem.addEventListener('click', function (e) {
+    console.log(e)
+    console.log(carouselAuto.style)
+    carouselAuto.style.transform = `translateX(-${width + 30}px)`
 })
 
 
+
+
+
+
+
+
+
+//form validation
+const regExps = {
+    email: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+    phone: /^\+?[0-9()-.\s]{10,}$/,
+    name: /[a-zA-Z]+/,
+    text: /^[a-zA-Z0-9\s.,;:'"&()!?]+$/,
+}
+
+const formA = document.querySelector('#formA')
+const formB = document.querySelector('#formB')
+//realtime warning
+function formValidator(event) {
+    const warning = this.querySelector('.warning')
+    let test = regExps[event.target.dataset.validator].test(event.target.value)
+    if (!test && !event.target.value == '') {
+        event.target.validationMessage = `Please enter valid ${event.target.dataset.validator}`
+        warning.innerText = `Please enter valid ${event.target.dataset.validator}`
+        warning.style.visibility = "initial"
+        event.target.style.borderColor = 'red'
+    } else {
+        warning.style.visibility = "hidden"
+        event.target.style.borderColor = 'var(--border-form)'
+    }
+}
+//submit blocker
+function allValidator(e) {
+    const warning = this.querySelector('.warning')
+    let inp = this.querySelectorAll('input, textarea')
+    let inputs = Array.from(inp)
+    let warnings = []
+    inputs.forEach(elem => {
+        if (!regExps[elem.dataset.validator].test(elem.value)) {
+            warnings.push(elem.dataset.validator)
+        }
+    })
+    if (warnings.length > 0) {
+        warning.innerText = `Please provide valid ${warnings.join(', ')}`
+        warning.style.visibility = "initial"
+        e.preventDefault()
+    } else {
+        warning.style.visibility = "hidden"
+    }
+}
+formA.addEventListener('change', formValidator)
+formA.addEventListener('submit', allValidator)
+formB.addEventListener('change', formValidator)
+formB.addEventListener('submit', allValidator)
 //video popup
 const videoContainer = document.querySelector('#vid-popup')
 const playPause = document.querySelector('.controls')
